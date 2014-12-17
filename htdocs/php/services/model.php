6e6e8dbb24dcfc6c20542a30d20bff802da9b3e3
YOUNG <YOUNG@gmail.com>
2014-12-17 17:49:05 +0100

    Create CRUD for model

--- htdocs/php/services/model.php
+++ htdocs/php/services/model.php
@@ -18,13 +18,45 @@ class Model extends RestGeneric{
       ),
       'returnExample' => array('type' => RestGeneric::RETURN_EXAMPLE_DYNAMIC)
     ),
+    'getModels' => array(
+      'type' => array(RestGeneric::METHOD_GET, RestGeneric::METHOD_POST),
+      'description' => '',
+      'params' => array(
+        'action' => array(
+          'required' => false,
+          'type' => RestGeneric::PARAM_TYPE_STRING,
+          'description' => '',
+          'exampleValue' => ''
+        )
+      ),
+      'returnExample' => array('type' => RestGeneric::RETURN_EXAMPLE_DYNAMIC)
+    ),
   );
 
   function createModel($request){
+    global $db;
+
     if (serviceHelper::isValidRequest($request['action'])) {
-      
+      $action = json_decode($request['action']);
+      if ($action == null) { return serviceHelper::returnMessage("missing");}
+      $model_name = $action->model_name;
+      $model_description = $action->model_description;
+      $model_image = $action->model_image;
+      $sql = "INSERT INTO model (model_name,model_description,model_image) VALUES ('{$model_name}','{$model_description}','{$model_image}');";
+      if (serviceHelper::notExistIn($model_name,"model_name","model")) {
+        $db->query($sql);
+        return serviceHelper::returnMessage("success");
+      }else{
+        return serviceHelper::returnMessage("existing");
+      }
     }else{
-      return serviceHelper::returnFalseMessage("missing");
+      return serviceHelper::returnMessage("missing");
     }
   }
+
+  function getModels($request){
+    $models = serviceHelper::query("SELECT * FROM model");
+    return $models;
+  }
+
 }
--- htdocs/php/utils/servicehelper.php
+++ htdocs/php/utils/servicehelper.php
@@ -2,18 +2,40 @@
 
 class serviceHelper{
   private static $messages = array(
-      'missing' => array('code' => '000', 'message' => 'Missing params'),
+      'success'  => array("code" => "111", "message" => "success"),
+      'missing'  => array('code' => '000', 'message' => 'Missing params'),
+      'existing' => array('code' => '001', 'message' => 'Param is already exist in database'),
+      
     );
 
   public static function isValidRequest($request){
-    if (!isset($request)) {
+    if (!isset($request) || $request == null) {
       return false;
     }else{
       return true;
     }
   }
 
-  public static function returnFalseMessage($keyword){
+  public static function returnMessage($keyword,$var){
     return self::$messages[$keyword];
+  }
+
+  public static function notExistIn($value,$attr,$table){
+    global $db;
+
+    $sql = "SELECT {$attr} FROM {$table} WHERE {$attr} = '{$value}';";
+    $result = self::query($sql);
+
+    if (count($result) > 0) {
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  public static function query($sql){
+    global $db;
+    $result = $db->query($sql,false,true);
+    return $result;
   }
 }
