099be3b3616b8916e165fa46f22b449f3c625e23
whysama <whysama@users.noreply.github.com>
2014-12-19 02:00:40 +0100

    Try to design the fronts

--- htdocs/app/scripts/app.js
+++ htdocs/app/scripts/app.js
@@ -16,7 +16,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/login.html',
         controller: 'MainCtrl'
       })
       .when('/musee', {
